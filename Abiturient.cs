19e1a97b4ef39a06b9b1aed3b955e93cf4b84069
Vovka0777 <94632891+Vovka0777@users.noreply.github.com>
2025-04-03 10:05:02 +0300

    Add files via upload

---
+++ Abiturient.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ConsoleApp32
+{
+    internal class Abiturient : Persona
+    {
+
+        public Abiturient(string name, DateTime data, string facult, int age) : base(name, data, facult, age)
+        {
+            this.Name = name;
+            this.Data = data;
+            this.Facult = facult;
+            this.Age = age;
+        }
+
+        public static Abiturient Enter()
+        {
+         Console.Clear();
+         Console.Write("Введите имя - ");
+         string name = Console.ReadLine();
+         Console.Write("Введите дату - ");
+         DateTime data = DateTime.Parse(Console.ReadLine());
+         Console.Write("Введите факультет - ");
+         string facut = Console.ReadLine();
+         Console.Write("Введите возраст - ");
+         int age = int.Parse(Console.ReadLine());
+
+         return new Abiturient(name, data, facut, age);
+        }
+
+        public override void Info()
+        {
+            Console.WriteLine($"Имя: {Name}\nДата рождения: {Data}\nФакультет: {Facult}\nВозраст: {Age}");
+        }
+
+    }
+
+}
+
---
+++ Persona.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp32
+{
+    abstract class Persona
+    {
+        private string name;
+        private DateTime data;
+        private string facult;
+        private int age;
+
+        public string Name { get => name; set => name = value; }
+        public DateTime Data { get => data; set => data = value; }
+        public string Facult { get => facult; set => facult = value; }
+        public int Age { get => age; set => age = value; }
+
+        public Persona (string name, DateTime data, string facult, int age)
+        {
+            this.name = name;
+            this.data = data;
+            this.facult = facult;
+            this.age = age;
+        }
+        public abstract void Info();
+
+    }
+}
